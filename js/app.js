a266a9e71a3d7685a54172bebb43e8f172d31b5e
Andi <andi@andi.andi>
2018-08-18 14:44:12 +0200

    Compiled app.js with babel to add support for older browsers

--- js/app.js
+++ js/app.js
@@ -1,35 +1,33 @@
-const navBar = document.getElementById("nav");
-const toTop = document.getElementById("to-top");
-const scrollbar = document.getElementById('scrollbar');
-const boxes = document.getElementsByClassName('box');
+"use strict";
 
+var navBar = document.getElementById("nav");
+var toTop = document.getElementById("to-top");
+var scrollbar = document.getElementById('scrollbar');
+var boxes = document.getElementsByClassName('box');
 
-navBar.addEventListener("click", function(e) {
+navBar.addEventListener("click", function (e) {
   // console.log(e.target.parentNode.parentNode);
-  if((e.target.className.includes("btn"))){
-  	removeActiveClass();
-  	// Append "active" class to target li.
-  	e.target.className += " active"; 	
+  if (e.target.className.includes("btn")) {
+    removeActiveClass();
+    // Append "active" class to target li.
+    e.target.className += " active";
   }
-  
 });
 
-function removeActiveClass(){
-	// Removes all other instances of "active" class from li 
-  	let navDiv = document.getElementById("nav");
-  	let items = navDiv.getElementsByTagName("li");
-  	for(let i = 0; i < items.length; i++){
-  			let activeElement = items[i].firstElementChild;
-  			items[i].firstElementChild.className = activeElement.className.replace("active", "");
-  		
-  	}
+function removeActiveClass() {
+  // Removes all other instances of "active" class from li
+  var navDiv = document.getElementById("nav");
+  var items = navDiv.getElementsByTagName("li");
+  for (var i = 0; i < items.length; i++) {
+    var activeElement = items[i].firstElementChild;
+    items[i].firstElementChild.className = activeElement.className.replace("active", "");
+  }
 }
 
 document.addEventListener('scroll', scrollTopButton);
 document.addEventListener('scroll', scrollbarProgress);
 
-
-function scrollTopButton(){
+function scrollTopButton() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("to-top").style.display = "block";
   } else {
@@ -37,10 +35,9 @@ function scrollTopButton(){
   }
 }
 
-function scrollbarProgress(){
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let scrolled = (winScroll/height) * 100;
+function scrollbarProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = winScroll / height * 100;
   scrollbar.style.width = scrolled + '%';
 }
-
